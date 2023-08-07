0bfccf509d376fa4380360d5be3b1ab8403c3956
jonnyACCI <jshan9078@gmail.com>
2023-08-07 17:51:41 -0400

    env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -35,3 +36,4 @@ module.exports = {
     }),
 ],
 }
+
